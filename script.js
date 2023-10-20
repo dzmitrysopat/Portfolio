2857d54193525ad986535399f1bfa4b6fe82e2a8
Dzmitry Sopat <dzmitrysopat@gmail.com>
2023-10-20 13:29:20 +0300

    main

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function show(){
+    document.getElementById("educationBtn").classList.remove("active");
+    document.getElementById("workBtn").classList.add("active");    
     document.getElementById("education").classList.remove("show");
     document.getElementById("education").classList.add("hide");
     document.getElementById("work").classList.add("show");
     document.getElementById("work").classList.remove("hide");
-    
 }
 
 function hide(){
+    document.getElementById("educationBtn").classList.add("active");
+    document.getElementById("workBtn").classList.remove("active");    
     document.getElementById("education").classList.add("show");
     document.getElementById("education").classList.remove("hide");
     document.getElementById("work").classList.remove("show");
-    document.getElementById("work").classList.add("hide");
+    document.getElementById("work").classList.add("hide"); 
 }
+
