8916cc41263baaf7fb4a77054f74b9cf1c675a26
Dzmitry Sopat <dzmitrysopat@gmail.com>
2023-10-30 13:04:59 +0300

    main

--- theme.js
+++ theme.js
@@ -14,18 +14,24 @@ document.querySelector('.theme').addEventListener('click', (event) => {
 
   function addDarkClassToHTML() {
     const image = document.getElementById("logoImg");
-    console.log(image.src);
+    const figma = document.getElementById("figma");
+    const git = document.getElementById("git");
+
     try {
       if (localStorage.getItem('theme') === 'dark') {
         document.querySelector('html').classList.add('dark');
         document.querySelector('header nav i').classList.add("uil-sun");
         document.querySelector('header nav i').classList.remove("uil-moon");
         image.src = './img/logo-light.png';
+        figma.src = 'https://skillicons.dev/icons?i=figma&theme=light';
+        git.src = 'https://skillicons.dev/icons?i=github&theme=light'
       }
       else {
         document.querySelector('html').classList.remove('dark');
         document.querySelector('header nav i').classList.add("uil-moon");
         document.querySelector('header nav i').classList.remove("uil-sun");
+        figma.src = 'https://skillicons.dev/icons?i=figma';
+        git.src = 'https://skillicons.dev/icons?i=github'
         image.src = 'img/logo.png';
       }
     } catch (err) { }
