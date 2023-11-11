36fa72acc0ea020b4b13b5630b802f46d85b2c1a
Dzmitry Sopat <dzmitrysopat@gmail.com>
2023-11-11 14:34:18 +0300

    main

--- js.js
+++ js.js
@@ -8,6 +8,8 @@ function show(){
     for (const school of education){
         school.classList.add("hidden");
     }
+    document.getElementById("WorkBtn").classList.add("active");
+    document.getElementById("EducationBtn").classList.remove("active");
 }
 
 function hide(){
@@ -17,4 +19,7 @@ function hide(){
     for (const school of education){
         school.classList.remove("hidden");
     }
+
+    document.getElementById("WorkBtn").classList.remove("active");
+    document.getElementById("EducationBtn").classList.add("active");
 }
