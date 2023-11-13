734a16f11eaba9a5617b220708bcbcb101c2b236
Dzmitry Sopat <dzmitrysopat@gmail.com>
2023-11-13 12:54:59 +0300

    main

--- js.js
+++ js.js
@@ -48,3 +48,31 @@ function hide(){
     document.getElementById("WorkBtn").classList.remove("active");
     document.getElementById("EducationBtn").classList.add("active");
 }
+
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+      document.querySelector("header nav a[href*=" + "home-anchor" + "]").classList.add("active-link");
+    });
+    if (localStorage.getItem('theme') != 'dark') {
+    scrollProgress.style.background = `conic-gradient(#f7941d ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+    }
+    else{
+      scrollProgress.style.background = `conic-gradient(#F7941D ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+    }
+  };
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
