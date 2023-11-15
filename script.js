255ebb068d92a0f1f002ae6d81a4ecebb1ac0d51
Dzmitry Sopat <dzmitrysopat@gmail.com>
2023-11-15 15:59:17 +0300

    main

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let articles = document.querySelectorAll("article[id]");
+console.log(articles);
 window.addEventListener("scroll", navHighlighter);
 function navHighlighter() {
   let scrollY = window.scrollY;
@@ -10,9 +11,9 @@ function navHighlighter() {
       scrollY > articleTop &&
       scrollY <= articleTop + articleHeight
     ){
-      document.querySelector("header nav a[href*=" + articleId + "-anchor" + "]").classList.add("active-link");
+      document.querySelector("header nav a[href*="+ articleId + "-anchor" + "]").classList.add("active-link");
     } else {
-      document.querySelector(".navigation a[href*=" + articleId + "-anchor" + "]").classList.remove("active-link");
+      document.querySelector("header nav a[href*="+ articleId + "-anchor" + "]").classList.remove("active-link");
     }
   });
 }
@@ -21,72 +22,115 @@ var swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
         type: 'bullets',
     },
-
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
     autoplay: {
         delay: 5000,
     },
-
     keyboard: {
         enabled: true,
         onlyInViewport: false,
     },
 });
 
+
+const education = document.querySelectorAll(".education");
+const work = document.querySelectorAll(".work");
+
 function show(){
-    document.getElementById("educationBtn").classList.remove("active");
-    document.getElementById("workBtn").classList.add("active");    
-    document.getElementById("education").classList.remove("show");
-    document.getElementById("education").classList.add("hidden");
-    document.getElementById("work").classList.add("show");
-    document.getElementById("work").classList.remove("hidden");
+    for (const worker of work){
+        worker.classList.remove("hidden");
+    }
+    for (const school of education){
+        school.classList.add("hidden");
+    }
+    document.getElementById("WorkBtn").classList.add("active");
+    document.getElementById("EducationBtn").classList.remove("active");
 }
 
 function hide(){
-    document.getElementById("Education").classList.add("active");
-    document.getElementById("Work").classList.remove("active");    
-    document.getElementById("Education").classList.add("show");
-    document.getElementById("Education").classList.remove("hidden");
-    document.getElementById("Work").classList.remove("show");
-    document.getElementById("work").classList.add("hidden"); 
+    for (const worker of work){
+        worker.classList.add("hidden");
+    }
+    for (const school of education){
+        school.classList.remove("hidden");
+    }
+
+    document.getElementById("WorkBtn").classList.remove("active");
+    document.getElementById("EducationBtn").classList.add("active");
 }
 
-let calcScrollValue = () => {
-  let scrollProgress = document.getElementById("progress");
-  let progressValue = document.getElementById("progress-value");
-  let pos = document.documentElement.scrollTop;
-  let calcHeight =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  let scrollValue = Math.round((pos * 100) / calcHeight);
-  if (pos > 100) {
-    scrollProgress.style.display = "grid";
-  } else {
-    scrollProgress.style.display = "none";
-  }
-  scrollProgress.addEventListener("click", () => {
-    document.documentElement.scrollTop = 0;
-    document.querySelector("header nav a[href*=" + "home-anchor" + "]").classList.add("active-link");
-  });
-  if (localStorage.getItem('theme') != 'dark') {
-  scrollProgress.style.background = `conic-gradient(#383838 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
-  }
-  else{
-    scrollProgress.style.background = `conic-gradient(#F7941D ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
-  }
-};
-window.onscroll = calcScrollValue;
-window.onload = calcScrollValue;
 
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+      document.querySelector("header nav a[href*=" + "home-anchor" + "]").classList.add("active-link");
+    });
+    if (localStorage.getItem('theme') != 'dark') {
+    scrollProgress.style.background = `conic-gradient(#f7941d ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+    }
+    else{
+      scrollProgress.style.background = `conic-gradient(#F7941D ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+    }
+  };
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
+
+  // function nav(){
+  //   document.getElementById("menu").classList.add("slide-top");
+  // }
+
+  document.querySelector('.uil-apps').addEventListener('click', (event) => {
+    document.getElementById("menu").classList.add("slide-top");
+    document.getElementById("menu").classList.add("burger-menu");
+  });
+
+  document.querySelector('.uil-times').addEventListener('click', (event) => {
+    document.getElementById("menu").classList.remove("slide-top");
+    document.getElementById("menu").classList.remove("burger-menu");
+  });
+
+function burgerNav(){
+    // document.querySelector('mob-link').addEventListener('click', (event) => {
+    document.getElementById("menu").classList.remove("burger-menu");
+  // });
+}
+
+setTimeout(function(){
+	document.body.classList.add('body_visible');
+}, 0);
+
+function onEntry(entry) {
+	entry.forEach(change => {
+	  if (change.isIntersecting) {
+		change.target.classList.add('element-show');
+	  }
+	});
+  }
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  for (let elm of elements) {
+	observer.observe(elm);
+  }
