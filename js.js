75e9f5e6c65957c5cfd6a5eb95aa2e059fc3d718
Dzmitry Sopat <dzmitrysopat@gmail.com>
2023-11-15 13:37:51 +0300

    main

--- js.js
+++ js.js
@@ -116,3 +116,21 @@ function burgerNav(){
     document.getElementById("menu").classList.remove("burger-menu");
   // });
 }
+
+setTimeout(function(){
+	document.body.classList.add('body_visible');
+}, 0);
+
+function onEntry(entry) {
+	entry.forEach(change => {
+	  if (change.isIntersecting) {
+		change.target.classList.add('element-show');
+	  }
+	});
+  }
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  for (let elm of elements) {
+	observer.observe(elm);
+  }
